42e59ca15f66fa94ecaad2d16ef2ef0da3249c67
GyapanJCU <gyapanshrestha@Gyapans-MacBook-Pro.local>
2021-01-06 12:45:39 +1000

    changes in footer

--- wp-content/themes/wprig-2.0/template-parts/footer/info.php
+++ wp-content/themes/wprig-2.0/template-parts/footer/info.php
@@ -10,16 +10,16 @@ namespace WP_Rig\WP_Rig;
 ?>
 
 <div class="site-info">
-	<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'wp-rig' ) ); ?>">
+	<a href="<?php echo esc_url( __( 'https://u3atownsville.com/', 'cmsa2v3' ) ); ?>">
 		<?php
 		/* translators: %s: CMS name, i.e. WordPress. */
-		printf( esc_html__( 'Proudly powered by %s', 'wp-rig' ), 'WordPress' );
+		printf( esc_html__( '2021 © U3A ', 'cmsa2v3' ) );
 		?>
 	</a>
 	<span class="sep"> | </span>
 	<?php
 	/* translators: Theme name. */
-	printf( esc_html__( 'Theme: %s by the contributors.', 'wp-rig' ), '<a href="' . esc_url( 'https://github.com/wprig/wprig/' ) . '">WP Rig</a>' );
+	printf( esc_html__( 'Theme: %s .', 'cmsa2v3' ), '<a href="' . esc_url( 'https://github.com/jcubcms/assessment-2-GyapanJCU.git' ) . '">cmsa2v3</a>' );
 
 	if ( function_exists( 'the_privacy_policy_link' ) ) {
 		the_privacy_policy_link( '<span class="sep"> | </span>' );
